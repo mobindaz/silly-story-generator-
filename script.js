1eb76c303138f448b03b586cfb0cb074a91456a2
mobindas m <146184535+mobindaz@users.noreply.github.com>
2023-11-26 08:40:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-const lifeElements = ["sunshine", "rain", "blossoming flowers", "laughter", "learning", "challenges", "friendship", "love", "adventure", "reflection"];
-const lifeActions = ["nurtured", "experienced", "overcame", "embraced", "discovered", "appreciated", "celebrated", "shared", "cherished", "remembered"];
+function generatePersonalStory() {
+    const userName = document.getElementById('name-input').value.trim();
 
-function getRandomElement(arr) {
-    const randomIndex = Math.floor(Math.random() * arr.length);
-    return arr.splice(randomIndex, 1)[0];
-}
+    if (!userName) {
+        alert('Please enter your name.');
+        return;
+    }
 
-function generateLifeStory() {
+    const lifeElements = ["sunshine", "rain", "blossoming flowers", "laughter", "learning", "challenges", "friendship", "love", "adventure", "reflection"];
+    const lifeActions = ["nurtured", "experienced", "overcame", "embraced", "discovered", "appreciated", "celebrated", "shared", "cherished", "remembered"];
+
+    function getRandomElement(arr) {
+        const randomIndex = Math.floor(Math.random() * arr.length);
+        return arr.splice(randomIndex, 1)[0];
+    }
+
     const element1 = getRandomElement(lifeElements);
     const action1 = getRandomElement(lifeActions);
     const element2 = getRandomElement(lifeElements);
@@ -18,13 +25,16 @@ function generateLifeStory() {
     lifeElements.push(...lifeElements);
     lifeActions.push(...lifeActions);
 
-    const lifeStory = `
-        In the journey of life, we've encountered ${element1} that ${action1} us.
-        Along the way, we've ${action2} ${element2} and faced ${element3} with resilience.
-        Life is a beautiful tapestry woven with moments of ${action3} ${element3}.
-        Each step is a chapter, and the adventure continues.`;
+    const personalStory = `
+        Dear ${userName},
+        In the journey of life, you've encountered ${element1} that ${action1} you.
+        Along the way, you've ${action2} ${element2} and faced ${element3} with resilience.
+        Your life is a beautiful tapestry woven with moments of ${action3} ${element3}.
+        Each step is a chapter, and the adventure continues.
+        Sincerely,
+        Your Personal Story Generator`;
 
-    document.getElementById('life-story-container').innerText = lifeStory;
+    document.getElementById('personal-story-container').innerText = personalStory;
 }
 
-document.getElementById('generate-btn').addEventListener('click', generateLifeStory);
+document.getElementById('generate-btn').addEventListener('click', generatePersonalStory);
