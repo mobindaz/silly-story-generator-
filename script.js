d4b1916c8afb8fafef6da667e86efae907d531c7
mobindas m <146184535+mobindaz@users.noreply.github.com>
2023-11-26 08:28:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
-const characters = ["The Ninja", "The Pirate", "The Unicorn", "The Robot"];
-const actions = ["ate", "danced with", "flew around with", "tickled"];
-const objects = ["a talking donut", "a rainbow", "a giant pickle", "a dancing chicken"];
-const locations = ["in a magical forest", "on top of a mountain", "underwater", "in outer space"];
-const emotions = ["laughing uncontrollably", "crying tears of joy", "doing a happy dance", "singing a silly song"];
-
 function getRandomElement(arr) {
     const randomIndex = Math.floor(Math.random() * arr.length);
-    return arr[randomIndex];
+    return arr.splice(randomIndex, 1)[0];
 }
 
 function generateSillyStory() {
-    const character = getRandomElement(characters);
-    const action = getRandomElement(actions);
-    const object = getRandomElement(objects);
-    const location = getRandomElement(locations);
-    const emotion = getRandomElement(emotions);
+    // Make copies of the original arrays to avoid modifying the originals
+    const charactersCopy = characters.slice();
+    const actionsCopy = actions.slice();
+    const objectsCopy = objects.slice();
+    const locationsCopy = locations.slice();
+    const emotionsCopy = emotions.slice();
+
+    const character = getRandomElement(charactersCopy);
+    const action = getRandomElement(actionsCopy);
+    const object = getRandomElement(objectsCopy);
+    const location = getRandomElement(locationsCopy);
+    const emotion = getRandomElement(emotionsCopy);
+
+    // Reset the arrays to their original state for the next story
+    charactersCopy.push(...characters);
+    actionsCopy.push(...actions);
+    objectsCopy.push(...objects);
+    locationsCopy.push(...locations);
+    emotionsCopy.push(...emotions);
 
     const sillyStory = `
         Once upon a time, ${character} ${action} ${object}.
         They were located ${location}, and they couldn't stop ${emotion}.
         It was a sight to behold as ${character} and ${object} became best friends.
-        But suddenly, a wild ${getRandomElement(characters)} appeared and joined the party!
-        Now, they were all ${action} ${getRandomElement(objects)} together.
+        But suddenly, a wild ${getRandomElement(charactersCopy)} appeared and joined the party!
+        Now, they were all ${action} ${getRandomElement(objectsCopy)} together.
         The end – or is it?`;
 
     document.getElementById('silly-story-container').innerText = sillyStory;
