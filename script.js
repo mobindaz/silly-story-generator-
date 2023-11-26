59b44d33eaa5fcf44ee6388eecec5beb6e85b528
mobindas m <146184535+mobindaz@users.noreply.github.com>
2023-11-26 08:20:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
-// Arrays of silly story elements
 const characters = ["The Ninja", "The Pirate", "The Unicorn", "The Robot"];
 const actions = ["ate", "danced with", "flew around with", "tickled"];
 const objects = ["a talking donut", "a rainbow", "a giant pickle", "a dancing chicken"];
+const locations = ["in a magical forest", "on top of a mountain", "underwater", "in outer space"];
+const emotions = ["laughing uncontrollably", "crying tears of joy", "doing a happy dance", "singing a silly song"];
 
-// Function to generate a random element from an array
 function getRandomElement(arr) {
     const randomIndex = Math.floor(Math.random() * arr.length);
     return arr[randomIndex];
 }
 
-// Function to generate a silly story
 function generateSillyStory() {
     const character = getRandomElement(characters);
     const action = getRandomElement(actions);
     const object = getRandomElement(objects);
+    const location = getRandomElement(locations);
+    const emotion = getRandomElement(emotions);
 
-    const sillyStory = `${character} ${action} ${object}.`;
+    const sillyStory = `
+        Once upon a time, ${character} ${action} ${object}.
+        They were located ${location}, and they couldn't stop ${emotion}.
+        It was a sight to behold as ${character} and ${object} became best friends.
+        But suddenly, a wild ${getRandomElement(characters)} appeared and joined the party!
+        Now, they were all ${action} ${getRandomElement(objects)} together.
+        The end – or is it?`;
 
-    // Display the silly story on the page
     document.getElementById('silly-story-container').innerText = sillyStory;
 }
 
-// Event listener for the button click
 document.getElementById('generate-btn').addEventListener('click', generateSillyStory);
