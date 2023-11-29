386ea7251a85166f39c32483694ad09f1bf5b21a
mobindas m <146184535+mobindaz@users.noreply.github.com>
2023-11-29 07:37:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,48 @@
-function generatePersonalStory() {
-    const userName = document.getElementById('name-input').value.trim();
+var customName = document.getElementById('customname');
+var randomize = document.querySelector('.randomize');
+var story = document.querySelector('.story');
 
-    if (!userName) {
-        alert('Please enter your name.');
-        return;
-    }
-
-    const lifeElements = ["sunshine", "rain", "blossoming flowers", "laughter", "learning", "challenges", "friendship", "love", "adventure", "reflection"];
-    const lifeActions = ["nurtured", "experienced", "overcame", "embraced", "discovered", "appreciated", "celebrated", "shared", "cherished", "remembered"];
-
-    function getRandomElement(arr) {
-        const randomIndex = Math.floor(Math.random() * arr.length);
-        return arr.splice(randomIndex, 1)[0];
-    }
-
-    const element1 = getRandomElement(lifeElements);
-    const action1 = getRandomElement(lifeActions);
-    const element2 = getRandomElement(lifeElements);
-    const action2 = getRandomElement(lifeActions);
-    const element3 = getRandomElement(lifeElements);
-    const action3 = getRandomElement(lifeActions);
-
-    // Reset the arrays to their original state for the next story
-    lifeElements.push(...lifeElements);
-    lifeActions.push(...lifeActions);
-
-    const personalStory = `
-        Dear ${userName},
-        In the journey of life, you've encountered ${element1} that ${action1} you.
-        Along the way, you've ${action2} ${element2} and faced ${element3} with resilience.
-        Your life is a beautiful tapestry woven with moments of ${action3} ${element3}.
-        Each step is a chapter, and the adventure continues.
-        
-        Sincerely,
-        Story Generator`;
-
-    document.getElementById('personal-story-container').innerText = personalStory;
+function randomValueFromArray(array) {
+  return array[Math.floor(Math.random() * array.length)];
 }
 
-document.getElementById('generate-btn').addEventListener('click', generatePersonalStory);
+var storyText =
+  "It was 94 farenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but he was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+
+var insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+
+var insertY = ["the soup kitchen", "Disneyland", "the White House"];
+
+var insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
+
+randomize.addEventListener('click', result);
+
+function result() {
+  var newStory = storyText;
+  var xItem = randomValueFromArray(insertX);
+  var yItem = randomValueFromArray(insertY);
+  var zItem = randomValueFromArray(insertZ);
+
+  var newStory = newStory.replace(':insertx:', xItem);
+  var newStory = newStory.replace(':insertx:', xItem);
+  var newStory = newStory.replace(':inserty:', yItem);
+  var newStory = newStory.replace(':insertz:', zItem);
+ 
+  if (customName.value != '') {
+    var name = customName.value;
+    newStory = newStory.replace('Bob', name);
+
+  }
+
+  if (document.getElementById("uk").checked) {
+    var weight = Math.round(300 * 0.0714286) + ' stone';
+    var temperature = Math.round((94 - 32) * (5/9)) + ' centigrade';
+    var newStory = newStory.replace('94 farenheit', temperature);
+    var newStory = newStory.replace('300 pounds', weight);
+
+  }
+
+ 
+  story.textContent = newStory;
+  story.style.visibility = 'visible';
+}
